6f70d4c1607b7e746a7747059ff764fb2ba89aec
ariel <ariel@insterior.co.kr>
2022-07-11 11:18:10 +0900

    처음 settting

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+const babelConfig = () => {
+  const presets = ['next/babel'];
+  const plugins = [['styled-components', { ssr: true, displayName: true }]];
+
+  if (process.env.NEXT_PUBLIC_STAGE === 'production') {
+    plugins.push([
+      'transform-remove-console',
+      { exclude: ['assert', 'debug', 'error', 'warn', 'info', 'trace'] },
+    ]);
+  }
+
+  return {
+    presets,
+    plugins,
+  };
+};
+
+module.exports = babelConfig();
